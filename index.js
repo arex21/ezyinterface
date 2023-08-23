4db33b1666c9b199551a66d8eed5db531a84cc76
Arex360 <122282190+arex520@users.noreply.github.com>
2023-08-23 21:48:28 +0500

    ddd

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 3010;
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3()
+const CyclicDb = require("@cyclic.sh/dynamodb")
+const db = CyclicDb("poised-gray-crabCyclicDB")
+const database = db.collection("db")
 app.get('/:cid/:filename', (req, res) => {
   const {cid,filename} = req.params
   res.redirect(`https://${cid}.ipfs.w3s.link/${filename}`);
@@ -10,20 +11,13 @@ app.get('/:cid/:filename', (req, res) => {
 app.get('/',(req,res)=>res.send("OK"))
 app.get('/save/:cid/:hash',async (req,res)=>{
     const {cid,hash} = req.params
-    await s3.putObject({
-        Body: JSON.stringify({key:hash}),
-        Bucket: "cyclic-poised-gray-crab-ap-south-1",
-        Key:   `some_files/${cid}.json`,
-    }).promise()
+    await database.set(cid,{hash})
     res.send("done")
 })
 app.get('/get/:cid/a/a',async(req,res)=>{
     const {cid} = req.params
-    let my_file = await s3.getObject({
-        Bucket: "cyclic-poised-gray-crab-ap-south-1",
-        Key: `some_files/${cid}.json`,
-    }).promise()
-    res.send(my_file)
+    const data = await database.get(cid)
+    res.send(data)
 })
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
