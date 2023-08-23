030ad43a20be5407174b63e440829f603cf48c83
Arex360 <122282190+arex520@users.noreply.github.com>
2023-08-23 22:42:59 +0500

    update

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const PORT = 3010;
 const CyclicDb = require("@cyclic.sh/dynamodb")
 const db = CyclicDb("poised-gray-crabCyclicDB")
 const database = db.collection("db")
-app.get('/:cid/:filename', async (req, res) => {
-  const {cid,filename} = req.params
+app.get('/:cid', async (req, res) => {
+  const {cid} = req.params
   const data = await database.get(cid)
   let __cid =  data["props"]["hash"]
-  res.redirect(`https://${__cid}.ipfs.w3s.link/${filename}`);
+  let __filename = data["props"]["filename"]
+  res.redirect(`https://${__cid}.ipfs.w3s.link/${__filename}`);
 });
 app.get('/',(req,res)=>res.send("OK"))
-app.get('/save/:cid/:hash',async (req,res)=>{
-    const {cid,hash} = req.params
-    await database.set(cid,{hash})
+app.get('/save/:cid/:hash/:filename',async (req,res)=>{
+    const {cid,hash,filename} = req.params
+    await database.set(cid,{hash,filename})
     res.send("done")
 })
 app.get('/get/:cid/a/a',async(req,res)=>{
